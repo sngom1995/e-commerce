55ce899aaec394bda6a2f108b9602cd3bed9faa8
sngom1995 <ngomsamba75@gmail.com>
2024-06-29 15:20:42 +0000

    test jenkins configs

--- src/main/java/com/example/demo/controllers/CartController.java
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -59,7 +59,7 @@ public class CartController {
 	public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
-			log.error("User not found; {}", request.getUsername());
+			log.error("User notFound; {}", request.getUsername());
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		Optional<Item> item = itemRepository.findById(request.getItemId());
