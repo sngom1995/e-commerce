614b8076638c1cb7be8672374199cd14864b254e
sngom1995 <ngomsamba75@gmail.com>
2024-06-29 14:43:00 +0000

    add test

---
+++ src/test/java/com/example/demo/controllers/CartControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import junit.framework.TestCase;
+
+public class CartControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAddTocart() {
+    }
+
+    public void testRemoveFromcart() {
+    }
+}
---
+++ src/test/java/com/example/demo/controllers/OrderControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.demo.controllers;
+
+import junit.framework.TestCase;
+
+public class OrderControllerTest extends TestCase {
+
+}
